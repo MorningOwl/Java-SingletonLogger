3c5df27b100107f7538e67a2371a40db6c228634
MorningOwl <dev.edw.calderon@gmail.com>
2017-09-14 12:22:22 -0400

    Synchornized the method to log file before it checks if a new file exists.
    Synchornized the method to log file before it checks if a new file exists. This prevents any early closure of streams while another thread is accessing it.

--- src/Logger/SingletonLogger.java
+++ src/Logger/SingletonLogger.java
@@ -16,27 +16,29 @@ public class SingletonLogger {
 	private String fileName;
 
 	/**
-	 * The constructor for SingletonLogger. Sets a fileName to be later
-	 * used for logging.
+	 * The constructor for SingletonLogger. Sets a fileName to be later used for
+	 * logging.
 	 */
-	private SingletonLogger(){
-		DateFormat dateFormat = new SimpleDateFormat("MMddyyyy");
+	private SingletonLogger() {
+		DateFormat dateFormat = new SimpleDateFormat("MMddyyyyHHmmss");
 		Date date = new Date();
-		fileName = "./outfile" + dateFormat.format(date)+ ".log";
+		fileName = "./outfile" + dateFormat.format(date) + ".log";
 	}
-	
+
 	/**
-	 * This returns the SingletonLogger instance. A double-checked
-	 * locking method implemented where it first checks if the instance was already 
-	 * initiated. If so, it returns an instance. If not, it obtains a lock (important
-	 * for multi-threading) and then checks again with the same condition. If it 
-	 * was already created by another thread, it just simply returns the instance.
+	 * This returns the SingletonLogger instance. A double-checked locking
+	 * method implemented where it first checks if the instance was already
+	 * initiated. If so, it returns an instance. If not, it obtains a lock
+	 * (important for multi-threading) and then checks again with the same
+	 * condition. If it was already created by another thread, it just simply
+	 * returns the instance.
+	 * 
 	 * @return A SingletonLogger instance of this class.
 	 */
-	public static SingletonLogger getInstance(){
-		if(instance == null){
-			synchronized(SingletonLogger.class){
-				if(instance == null){
+	public static SingletonLogger getInstance() {
+		if (instance == null) {
+			synchronized (SingletonLogger.class) {
+				if (instance == null) {
 					instance = new SingletonLogger();
 					System.out.println("First instance of SingletonLogger created.");
 				}
@@ -44,50 +46,63 @@ public class SingletonLogger {
 		}
 		return instance;
 	}
-	
+
 	/**
 	 * Invokes the System.out.println method.
+	 * 
 	 * @param obj
 	 */
-	public void println(Object obj){
+	public void println(Object obj) {
 		System.out.println(obj);
 	}
-	
+
 	/**
 	 * Invokes the System.out.print method.
+	 * 
 	 * @param obj
 	 */
-	public void print(Object obj){
+	public void print(Object obj) {
 		System.out.print(obj);
 	}
-	
-	public void logInFile(String s){
+
+	/**
+	 * Creates/appends an out file with the given string. The outfile is stored
+	 * in the same folder as the program in a file name that was set in the
+	 * constructor.
+	 * 
+	 * @param log
+	 *            The Object that will be logged in the file.
+	 */
+	public void logInFile(Object log) {
 		bwriter = null;
 		fwriter = null;
-		
-		try{
-			synchronized(SingletonLogger.class){
-				file = new File(fileName);						
+		boolean existingFile = true;
+
+		synchronized (SingletonLogger.class) {
+			try {
+				file = new File(fileName);
 				if (!file.exists()) {
 					file.createNewFile();
+					existingFile = false;
 				}
 
 				fwriter = new FileWriter(file.getAbsoluteFile(), true);
 				bwriter = new BufferedWriter(fwriter);
-				bwriter.write(s);
-
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (bwriter != null)
-					bwriter.close();
-				if (fwriter != null)
-					fwriter.close();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-
+				if (existingFile) {
+					bwriter.newLine();
+				}
+				bwriter.write(log.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (bwriter != null)
+						bwriter.close();
+					if (fwriter != null)
+						fwriter.close();
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
 			}
 		}
 	}
--- src/Logger/SingletonLoggerTest.java
+++ src/Logger/SingletonLoggerTest.java
@@ -1,11 +1,46 @@
 package Logger;
 
 public class SingletonLoggerTest {
+	
 
-	public static void main(String[] args) {
-		SingletonLogger log = SingletonLogger.getInstance();
-		
-		log.logInFile("test");
+	public static void main(String[] args) {			
+	    RunnableDemo R1 = new RunnableDemo( "Thread-1");
+	    R1.start();
+	    
+	    RunnableDemo R2 = new RunnableDemo( "Thread-2");
+	    R2.start(); 
 	}
-
 }
+
+
+class RunnableDemo implements Runnable {
+	   private Thread t;
+	   private String threadName;
+	   SingletonLogger log = SingletonLogger.getInstance();
+	   
+	   RunnableDemo( String name) {
+	      threadName = name;
+	      log.logInFile("Creating " +  threadName );
+	   }
+	   
+	   public void run() {
+		  log.logInFile("Running " +  threadName );
+	      try {
+	         for(int i = 4; i > 0; i--) {
+	        	 log.logInFile("Thread: " + threadName + ", " + i);
+	            Thread.sleep(50);
+	         }
+	      }catch (InterruptedException e) {
+	    	  log.logInFile("Thread " +  threadName + " interrupted.");
+	      }
+	      log.logInFile("Thread " +  threadName + " exiting.");
+	   }
+	   
+	   public void start () {
+		   log.logInFile("Starting " +  threadName );
+	      if (t == null) {
+	         t = new Thread (this, threadName);
+	         t.start ();
+	      }
+	   }
+	}
